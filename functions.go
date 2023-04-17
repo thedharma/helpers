600ea0216faf1aa3c112209446010c25c6f19957
thedharma <mark.donato99@yahoo.com.au>
2023-04-17 16:52:33 +1000

    add stringmapfrom2slices func

--- functions.go
+++ functions.go
@@ -1,6 +1,11 @@
 package helpers
 
-// Splits string into slice of strings at seperator which is not in quotes. 
+import (
+	"errors"
+	"fmt"
+)
+
+// Splits string into slice of strings at seperator which is not in quotes.
 // Seperator must be able to be represented by one byte. Otherwise will panic with index error.
 func SplitUnquoted(s, sep string) []string {
 	result := []string{}
@@ -24,3 +29,20 @@ func SplitUnquoted(s, sep string) []string {
 	}
 	return append(result, s[begin :])
 }
+
+func StringMapFrom2Slices(keys, values []string) (map[string]string, error) {
+	var stringMap = map[string]string{}
+	keyLength, valueLength := len(keys), len(values)
+
+	if len(keys) != len(values) {
+		errMsg := fmt.Sprintf("difference in amount of keys and values: %v != %v", keyLength, valueLength)
+		return stringMap, errors.New(errMsg)
+	}
+
+	for i := 0; i < keyLength; i++ {
+		stringMap[keys[i]] = values[i]  
+	}
+
+	return stringMap, nil
+}
+
